0d2e7d5129fe0d89a0a28d0c04b2a403460d56fd
cristhofert <cristhofert97@gmail.com>
2021-05-27 12:14:15 +0000

    add login view

---
+++ src/js/views/login.js
@@ -0,0 +1,31 @@
+import React, {
+    useState,
+    useContext
+} from "react";
+import { Link } from "react-router-dom";
+import { Context } from "../store/appContext";
+
+export const CharacterDetails = () => {
+    const { store, actions } = useContext(Context);
+
+    return (
+        <div className="container">
+            <div className="row">
+                <div className="col">
+                    <form>
+                        <div class="form-group">
+                            <label for="exampleInputEmail1">Email address</label>
+                            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"></input>
+                            <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+                        </div>
+                        <div class="form-group">
+                            <label for="exampleInputPassword1">Password</label>
+                            <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password"></input>
+                        </div>
+                        <button type="submit" class="btn btn-primary">Submit</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    );
+};
